474c98fe08347ac6fbc21678065fae4686053803
vivranj <40149420+vivranj@users.noreply.github.com>
2023-10-30 20:57:32 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)//we r using node js not browser
+
+console.log(3 
+    +3)//code readaility should be high
+
+
+console.log("vivek");
+
+let name="hitesh"//string
+let age=18//number
+let isLoggedIn=true//boolean
+
+//primitive data types
+
+//number - 2^53 range about
+//bigint
+//string - ""
+//boolean - true/false
+//null - standalone value
+//undefined
+//symbol - unique
+
+
+//object
+
+console.log(typeof "hit");
+console.log(typeof age);
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
